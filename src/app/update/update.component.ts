32d9e3dc0c282b4d249e2ffe2654a6c4fee51ac8
Akshay Patil <76992034+akshhpatil@users.noreply.github.com>
2021-03-01 17:34:48 +0530

    Create update.component.ts

---
+++ src/app/update/update.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Student } from '../student';
+import { ActivatedRoute, Router } from '@angular/router';
+import { StudentService } from '../student.service';
+
+
+@Component({
+  selector: 'app-update',
+  templateUrl: './update.component.html',
+  styleUrls: ['./update.component.css']
+})
+export class UpdateComponent implements OnInit {
+
+  id: number;
+  student: Student;
+
+  constructor(private route: ActivatedRoute,private router: Router,
+    private studentService: StudentService) { }
+
+    ngOnInit() {
+    
+      this.student = new Student();
+  
+      this.id = this.route.snapshot.params['id'];
+      
+      this.studentService.getStudent(this.id)
+        .subscribe(data => {
+          console.log(data)
+          this.student = data;
+        }, error => console.log(error));
+    }
+
+    updateStudent() {
+      this.studentService.updateStudent(this.id, this.student)
+        .subscribe(data => {
+          console.log(data);
+          this.student = new Student();
+          this.gotoList();
+        }, error => console.log(error));
+    }
+
+    onSubmit() {
+      this.updateStudent();    
+    }
+  
+    gotoList() {
+      this.router.navigate(['/stud']);
+    }
+
+}
