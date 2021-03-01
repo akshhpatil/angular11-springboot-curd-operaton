5738f4921cf5e09ec080a18d15e3d2dd69e17568
Akshay Patil <76992034+akshhpatil@users.noreply.github.com>
2021-03-01 17:36:30 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { StudListComponent } from './stud-list/stud-list.component';
+import { AddComponent } from './add/add.component';
+import { UpdateComponent } from './update/update.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'stud', pathMatch: 'full' },
+  { path: 'stud', component: StudListComponent },
+  { path: 'add', component:  AddComponent  },
+  { path: 'update/:id', component: UpdateComponent  },
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
