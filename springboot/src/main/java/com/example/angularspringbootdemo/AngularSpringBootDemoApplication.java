f918bf0190451aeb8b0da6742ee8b06790d6bd73
Akshay Patil <76992034+akshhpatil@users.noreply.github.com>
2021-03-01 17:37:00 +0530

    Create AngularSpringBootDemoApplication.java

---
+++ springboot/src/main/java/com/example/angularspringbootdemo/AngularSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.angularspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringBootDemoApplication.class, args);
+	}
+
+}
