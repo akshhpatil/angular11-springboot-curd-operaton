b8ced73623d6f56a674173492bf994edcc13a8cf
Akshay Patil <76992034+akshhpatil@users.noreply.github.com>
2021-03-01 17:37:41 +0530

    Create AngularSpringBootDemoApplicationTests.java

---
+++ springboot/src/test/java/com/example/angularspringbootdemo/AngularSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.angularspringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularSpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
